5a98d6afa228713407700e57cdfd8d602f11887c
Dave Mateer <davemateer@gmail.com>
2021-08-11 15:11:40 +0100

    auto

--- src/Pages/upload-test.cshtml.cs
+++ src/Pages/upload-test.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,9 +11,12 @@ namespace TusTest.Pages
 {
     public class UploadTestModel : PageModel
     {
+        public string TempPath { get; set; }
+
         public void OnGet()
         {
-
+            var tempPath = Path.GetTempPath();
+            TempPath = tempPath;
         }
     }
 }
